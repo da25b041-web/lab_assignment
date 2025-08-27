02ee291b637b1396a45a0cf9ebaa32162cae5552
Sakshi Sagun <da25b041@smail.iitm.ac.in>
2025-08-27 11:51:55 +0530

    This file contains the solutions.

---
+++ solution.sh
@@ -0,0 +1,12 @@
+sort -n numbers.txt
+ipconfig
+cat readme.txt
+wc -l data.csv
+grep -rl "error" logs
+tail -n 10 app.log
+chmod a+x script.sh
+grep "TODO" *.py
+history 20
+find ../../../ -type d -name "backup"
+sed 's/foo/bar/g' example.txt > new_example.txt
+
